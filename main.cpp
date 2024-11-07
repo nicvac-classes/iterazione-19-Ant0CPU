90bd646bbab5d6aa1ca18a038e11d307d4f5cba2
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-11-07 09:41:10 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,39 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n, q, r, d, Nd;
+
+    cout<<"Inserire un nuemero"<<endl;
+    cin>>n;
+
+    for(d=1; d<=n; d++)
+    {
+        q=n/d;
+
+        r=n%d;
+
+        if (r==0)
+        {
+            cout<<d<<"è divisore di"<<n<<endl;
+
+            Nd=Nd+1;
+        }
+        else
+        {
+            cout<<""<<d<<" non è divisore di "<<n<<endl;
+        }
+    }
+
+    cout<<"I divisori sono "<<Nd<<endl;
+
+    if (Nd==2)
+    {
+        cout<<n<<" è un numero primo"<<endl;
+    }
+    else
+    {
+        cout<<n<<" non è un numero primo"<<endl;
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
